681c0ce583f23c2ddd08b315337d365347b01c93
Daniil <daniil-go@protonmail.com>
2020-02-22 17:34:09 +0300

    чуть поправил комментарий к коду

--- lesson1/search_hw/search.go
+++ lesson1/search_hw/search.go
@@ -18,16 +18,16 @@ func main() {
 	}
 
 	var (
-		result = make([]string, 0, len(sites)) //создаем срез типа строка с кол-вом 0 элементов, но подготовленный
-												//для увелечения кол-ва элементов как в массиве sites
+		result = make([]string, 0, len(sites)) // создаем срез типа строка с кол-вом 0 элементов, но подготовленный
+												// для увелечения кол-ва элементов как в массиве sites
 		errs   int
 	)
 
 	// you can add flag here
 	if !true {
-		result, errs = search(str, sites) //запуск функции поиска v1
+		result, errs = search(str, sites) // запуск функции поиска v1
 	} else {
-		result, errs = searchConcurrency(str, sites) //запуск функции поиска v2 - первой
+		result, errs = searchConcurrency(str, sites) // запуск функции поиска v2 - первой
 	}
 
 	if errs > 0 { // подсчет и вывод ошибок
@@ -39,34 +39,34 @@ func main() {
 		return
 	}
 
-	log.Printf("Sites: %v", result) //вывод успешных результатов поиска
+	log.Printf("Sites: %v", result) // вывод успешных результатов поиска
 }
 
 // the first version of search()
 // реализация функции поиска v1
-func search(str string, sites []string) ([]string, int) { //принимает на вход строку запроса и массив сайтов, выводит
+func search(str string, sites []string) ([]string, int) { // принимает на вход строку запроса и массив сайтов, выводит
 	//срез стринг и число (ошибок)
-	out := make([]string, 0, 1) //подготовка среза для вывода результата
+	out := make([]string, 0, 1) // подготовка среза для вывода результата
 	errs := 0 // счетчик ошибок
 
-	for _, site := range sites { //запускается цикл, который итерируется по массиву сайтов
-		res, err := getReq(site) //запуск функции GET запроса. возвращет массив байтов Body
-		if err != nil { //обработка ошибок
+	for _, site := range sites { // запускается цикл, который итерируется по массиву сайтов
+		res, err := getReq(site) // запуск функции GET запроса. возвращет массив байтов Body
+		if err != nil { // обработка ошибок
 			errs++
-			log.Print(err) //вывод ошибок
+			log.Print(err) // вывод ошибок
 			continue
 		}
 
-		if strings.Contains(string(res), str) { //если в строке результата запроса содержится поисковый запрос,
+		if strings.Contains(string(res), str) { //е сли в строке результата запроса содержится поисковый запрос,
 			// то в срез out добавляется сайт, на котором этот запрос был найден
 			out = append(out, site)
 		}
 	}
 
-	return out, errs //возврат сайтов, на которых был найден поисковый запрос и количества ошибок
+	return out, errs // возврат сайтов, на которых был найден поисковый запрос и количества ошибок
 }
 
-func getReq(reqURL string) ([]byte, error) { //функция GET запроса принимает на вход адрес сайта
+func getReq(reqURL string) ([]byte, error) { // функция GET запроса принимает на вход адрес сайта
 	// и возвращает тело запроса в виде массива байтов и количество ошибок
 	resp, err := http.Get(reqURL) // GET запрос
 	if err != nil {
@@ -74,7 +74,7 @@ func getReq(reqURL string) ([]byte, error) { //функция GET запроса
 	}
 	defer resp.Body.Close()
 
-	body, err := ioutil.ReadAll(resp.Body) //чтение из тела запроса
+	body, err := ioutil.ReadAll(resp.Body) // чтение из тела запроса
 	if err != nil {
 		return nil, err
 	}
@@ -82,7 +82,7 @@ func getReq(reqURL string) ([]byte, error) { //функция GET запроса
 	return body, nil // возвращаем тело запроса и ничего
 }
 
-type chunk struct { //создаем структуру
+type chunk struct { // создаем структуру
 	site string
 	err  error
 }
@@ -90,34 +90,34 @@ type chunk struct { //создаем структуру
 // the second version of search
 func searchConcurrency(str string, sites []string) ([]string, int) { // тоже самое - на вход строка, массив сайтов,
 	// а на выход срез и кол-во ошибок
-	wg := sync.WaitGroup{} //присваем тип WaitGroup (будем ждать завершения всех горутин)
-	results := make(chan chunk) //создаем канал, который будет посылать данные в структуру
+	wg := sync.WaitGroup{} // присваем тип WaitGroup (будем ждать завершения всех горутин)
+	results := make(chan chunk) // создаем канал, который будет посылать данные в структуру
 
-	for _, site := range sites { //итерируемся по массиву сайтов
-		wg.Add(1) //добавляем 1 в группу ожидания горутин
-		go func(site string) { //запуск горутины
-			defer wg.Done() //по завершении данной горутины удаляем ее из группы ожидания
-			res, err := getReq(site) //запуск функции ГЕТ запроса
+	for _, site := range sites { // итерируемся по массиву сайтов
+		wg.Add(1) // добавляем 1 в группу ожидания горутин
+		go func(site string) { // запуск горутины
+			defer wg.Done() // по завершении данной горутины удаляем ее из группы ожидания
+			res, err := getReq(site) // запуск функции ГЕТ запроса
 			if err != nil {
-				results <- chunk{site: site, err: err} //отправляем ошибки в структуру
+				results <- chunk{site: site, err: err} // отправляем ошибки в структуру
 				return
 			}
 
 			if strings.Contains(string(res), str) {
-				results <- chunk{site: site} //отправляем найденное значение в структуру
+				results <- chunk{site: site} // отправляем найденное значение в структуру
 			}
-		}(site) //Вот эту запись я не понял
+		}(site) // анонимная функция с замыканием; передаем в нее значение site
 	}
 
 	go func() {
-		wg.Wait() //ждем, пока закончат работу все горутины
+		wg.Wait() // ждем, пока закончат работу все горутины
 		log.Println("channel closed")
-		close(results) //закрываем канал
+		close(results) // закрываем канал
 	}()
 
-	out := make([]string, 0, len(sites)) //готовим срез для вывода рез-татов
+	out := make([]string, 0, len(sites)) // готовим срез для вывода рез-татов
 	errs := 0
-	for chunk := range results { //только тут вроде бы должно быть наоборот: for results := range chunk
+	for chunk := range results { // results - канал, и мы из него читаем чанки, здесь chunk как элемент канала
 		if chunk.err != nil {
 			errs++
 			// log.Printf("Error %v for site %v", chunk.err, chunk.site)
