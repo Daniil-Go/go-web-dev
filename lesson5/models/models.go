5d791bcb2171cab3f7ed30167253db0d75b0ae24
Daniil <97057@mail.ru>
2020-03-15 13:21:42 +0300

    moved into dir

--- lesson5/models/models.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Post struct {
-	Id      int    `json:"id"`
-	Title   string `json:"title"`
-	Date    string `json:"date"`
-	Link    string `json:"link"`
-	Comment string `json:"comment"`
-}
--- lesson5/views/head.tpl
+++
@@ -1,4 +0,0 @@
-<head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.1.5/css/uikit.min.css" />
-</head>
