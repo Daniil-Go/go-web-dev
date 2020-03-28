78290fbf7174f47c31cb8728c89ef87df4ca83d7
Daniil <97057@mail.ru>
2020-03-28 17:15:22 +0300

    ad config and log

---
+++ lesson8/ratesServ/data.go
@@ -0,0 +1,30 @@
+package main
+
+import "sync"
+
+type Configuration struct {
+	EndpointUrl string `json:"endpoint_url" check:"required"`
+	Port        string `json:"port" check:"required"`
+	LogFile     string `json:"log_file" check:"required"`
+}
+
+type Cache struct {
+	Rates map[string]float64 `json:"rates"`
+	Base  string             `json:"base"`
+	Date  string             `json:"date"`
+	mu    sync.Mutex
+}
+
+type Response struct {
+	BaseName  string
+	BaseRate  float64
+	QuoteName string
+	QuoteRate float64
+	Rate      float64
+	Error     string
+}
+
+type SearchParams struct {
+	Base  string
+	Quote string
+}
